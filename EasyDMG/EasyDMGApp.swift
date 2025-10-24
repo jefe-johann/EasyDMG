71af25703e1a582833fb9a439a4b5f8b637ebc31
Jeff Schumann <jeff@Super-MacBook.local>
2025-10-24 15:55:34 -0400

    Initial Commit

---
+++ EasyDMG/EasyDMGApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyDMGApp.swift
+//  EasyDMG
+//
+//  Created by Jeff Schumann on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EasyDMGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
